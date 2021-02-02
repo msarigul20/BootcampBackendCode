03608c7c2a71daa01092841b3a1b21fb47cd4921
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-02 03:27:31 +0300

    Day6Homework2 Done.

---
+++ Day6Homework2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day6Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SQL was HERE!");
+            
+
+        }
+    }
+}
---
+++ Day6Homework2/SQLQuery.sql
@@ -0,0 +1,26 @@
+﻿Select	Products.ProductName as ÜrünAdı,		
+		--İndirim Uygulanmış fiyatın hesabı (unitprice Order Detailsden çekiliyor. {Hoca öyle istemiş.} )
+		--Ben örnek olarak "Northwoods Cranberry Sauce" ürününde çalıştım ve elle hesapladım.
+		--Product tablosundaki unitPrice sabit olup order details tablosunda unitPrice bazI order'larda değişiyor.
+		Sum(([Order Details].Quantity*[Order Details].UnitPrice)
+		-([Order Details].Quantity*[Order Details].UnitPrice)*[Order Details].Discount) 
+		as KazanılanToplamMiktar
+		/*
+			--İndirimsiz Fiyatın Hesabı 
+			Sum(([Order Details].Quantity*[Order Details].UnitPrice)) 
+			as KazanılanToplamMiktar
+		*/
+
+from Products left join   [Order Details]
+on [Order Details].ProductID = Products.ProductID
+group by Products.ProductName 
+order by Products.ProductName
+
+
+
+
+
+
+
+
+
