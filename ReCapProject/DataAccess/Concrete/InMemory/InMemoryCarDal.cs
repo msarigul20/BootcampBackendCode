24779f162c66dd87a8e71089d9852c194f9b4973
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-04 17:56:21 +0300

    Changed total number of cars that added in InMemoryCarDal.

--- ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -20,7 +20,7 @@ namespace DataAccess.Concrete.InMemory
                 new Car{CarId=2,BrandId=2,ColorId=2,DailyPrice=150, ModelYear="2019",Description= "Renault Clio Hatchback 4 Kapı 80.000km Benzin Düz Vites 22 Yaş Sınırı"},
                 new Car{CarId=3,BrandId=3,ColorId=1,DailyPrice=80, ModelYear="2016",Description= "Hyundai i20 Hatchback 4 Kapı 290.000km LPG-Benzin Düz Vites 18 Yaş Sınırı"},
                 new Car{CarId=4,BrandId=1,ColorId=3,DailyPrice=250, ModelYear="2020",Description= "Fiat 124 Spider Cabrio 2 Kapı 30.000km Benzin Otomatik Vites 22 Yaş Sınırı"},
-               // new Car{CarId=4,BrandId=4,ColorId=4,DailyPrice=225, ModelYear="2019",Description= "Nissan Qashqai SUV 4 Kapı 100.000km Dizel Düz Vites 20 Yaş Sınırı"}
+                new Car{CarId=4,BrandId=4,ColorId=4,DailyPrice=225, ModelYear="2019",Description= "Nissan Qashqai SUV 4 Kapı 100.000km Dizel Düz Vites 20 Yaş Sınırı"}
 
             };
         }
