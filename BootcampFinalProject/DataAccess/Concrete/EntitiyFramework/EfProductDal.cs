72e6bc656be02f3ff85d14dc80fc3f631a233573
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-06 01:54:35 +0300

    Uploaded lastest files.

---
+++ BootcampFinalProject/DataAccess/Concrete/EntitiyFramework/EfProductDal.cs
@@ -0,0 +1,71 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            //IDisposable pattern implementation of C# to gain memory speed by triggering garbage collector after terminating snippet.
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                //Catch the reference
+                var addedEntity = context.Entry(entity);
+                //Its object that will add.
+                addedEntity.State = EntityState.Added;
+                //Save all changes.(Really added.)
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                //Catch the reference
+                var deletedEntity = context.Entry(entity);
+                //Its object that will delete.
+                deletedEntity.State = EntityState.Deleted;
+                //Save all changes.(Really deleted.)
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                //Catch the reference
+                var updatedEntity = context.Entry(entity);
+                //Its object that will add.
+                updatedEntity.State = EntityState.Modified;
+                //Save all changes.(Really updated.)
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ BootcampFinalProject/DataAccess/Concrete/EntitiyFramework/NorthwindContext.cs
@@ -0,0 +1,30 @@
+﻿
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context means that to connect database tables with project classes such as Entities.
+    //Conrete classes by implementing (by inheriting from DbContext Class in Microsoft Entity Framework NuGet package) DbContext class.
+    public class NorthwindContext : DbContext
+    {
+        //The method provides to state that the project related to which database. 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Used '@' sign because of reverse slash.
+            //Server parameter takes address of database server for now in local.
+            //Database  parameter takes name of database.
+            //Trusted_Connection paramater provides to connect database server without username or password by taking true value.
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        //Initialization of tables and project entities classes.
+        //Which table will correspond which class.  
+        //project classes -  Db Tables
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
