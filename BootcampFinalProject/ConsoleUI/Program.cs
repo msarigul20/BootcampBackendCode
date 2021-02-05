7e91cbe6f427a4c4563d4ad7945b348e33a140ce
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-06 02:35:05 +0300

    Comments checked.

--- BootcampFinalProject/ConsoleUI/Program.cs
+++ BootcampFinalProject/ConsoleUI/Program.cs
@@ -10,11 +10,12 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-
+            // Products have unitprice between 40 and 100
             foreach (var product in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
+
 
 
 
