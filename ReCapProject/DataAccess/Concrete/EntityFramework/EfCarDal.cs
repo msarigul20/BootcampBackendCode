45787d1d3c3ee30ed7cfad51db412fd28f2991b7
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-06 14:28:20 +0300

    Coded EfCarDal with context and filter code. Added NuGet package for using EntityFramework

--- ReCapProject/DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,37 +1,66 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : ICarDal
     {
+
         public void Add(Car entity)
         {
-            throw new NotImplementedException();
+            //IDisposable pattern implementation of C# to gain memory speed by triggering garbage collector after terminating snippet.
+            using (CarDbContext context = new CarDbContext())
+            {
+                //Catch the reference
+                var addedEntity = context.Entry(entity);
+                //Its object that will add.
+                addedEntity.State = EntityState.Added;
+                //Save all changes.(Really added.)
+                context.SaveChanges();
+            }
         }
 
         public void Delete(Car entity)
         {
-            throw new NotImplementedException();
+            using (CarDbContext context = new CarDbContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public Car Get(Expression<Func<Car, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (CarDbContext context = new CarDbContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
         }
 
         public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (CarDbContext context = new CarDbContext())
+            {
+                return filter == null ? context.Set<Car>().ToList() : context.Set<Car>().Where(filter).ToList();
+            }
         }
 
         public void Update(Car entitiy)
         {
-            throw new NotImplementedException();
+            //IDisposable pattern implementation of C# to gain memory speed by triggering garbage collector after terminating snippet.
+            using (CarDbContext context = new CarDbContext())
+            {
+                var updatedEntity = context.Entry(entitiy);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
