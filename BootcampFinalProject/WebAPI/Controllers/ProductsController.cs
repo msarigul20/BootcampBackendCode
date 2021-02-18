e08a20fa1e1a038b72dd0507162a5e684bc14e44
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-18 13:48:13 +0300

    Added api layer in lesson (13.Feb.2021)

---
+++ BootcampFinalProject/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,75 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")] //BİZE NASIL İSTEKTE BULUNUCAKLAR
+    [ApiController] //Attribute  (C#)
+                    //Annotation (Java)
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //Naming Concention ([_]productService)
+        //IoC container (Inversion of Control) api kullanmak isteyen kişi productService ile çalışcanı 
+            //biliyor ama productservice nin ne olduğunu bilmiyor o yüzden çalışmıyor
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Swagger hazır dökümantasyon sağlayan yapı
+            //Dependency chain (BAD THING)
+            // IProductService needs ProductManager that needs EfProductDal:)
+            //IProductService productService = new ProductManager(new EfProductDal());
+
+
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+
+            }
+            return BadRequest(result);
+        }
+
+
+
+
+        [HttpPost("add")]
+        //Controllerin bildiği yer line 49 dur(IActionResult) MethodName(()
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
