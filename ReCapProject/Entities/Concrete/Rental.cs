5acb77f6b7ff53124fcc52fb45e683177a6d0d0a
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-18 13:29:25 +0300

    Added new entities

--- ReCapProject/Entities/Concrete/Rental.cs
+++ ReCapProject/Entities/Concrete/Rental.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using Core.Entities;
 
 namespace Entities.Concrete
 {
@@ -10,8 +10,7 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public int CarId { get; set; }
         public int CustomerId { get; set; }
-        public DateTime RentalDate { get; set; }
-        public DateTime ReturnDate { get; set; }
-
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
