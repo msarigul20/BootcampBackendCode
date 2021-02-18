5d3ef55f8dddc2be796541f104d3600c6e79128d
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-18 13:49:26 +0300

    Comment changes to test rules and all project.

--- BootcampFinalProject/Business/Concrete/ProductManager.cs
+++ BootcampFinalProject/Business/Concrete/ProductManager.cs
@@ -36,12 +36,12 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
 
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintainceTime);
             }
 
-            
+
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
         }
 
