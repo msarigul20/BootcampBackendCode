b9a723b1a6825edf8fb2d6746fe9cf55ef8f5868
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-25 14:56:59 +0300

    Business engine code with two ways.

---
+++ BootcampFinalProject/Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,26 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules 
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            //Diğer yöntem
+            //List<IResult> errorResults = new List<IResult>();
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                    //errorResults.Add(logic);
+                }
+            }
+            return null;
+            //return errorResults;
+        }
+    }
+}
