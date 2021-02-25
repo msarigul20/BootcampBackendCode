3c2c5020498975c90e97fcd2d3fbf7b64ad34b4a
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-02-25 14:56:06 +0300

    Refactored to work with Business Engine and Rules

--- BootcampFinalProject/Business/Concrete/ProductManager.cs
+++ BootcampFinalProject/Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
+
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -10,6 +12,7 @@ using Entities.DTOs;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -18,21 +21,66 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        ICategoryService _categoryService;
+        /* HATALI KOOOOOOOODDDDDDD
+         *          productManager                  categoryDal
+         * Bir Özel entityManager kendisinden başka dal enjekte edemez. 
+        IProductDal _categoryDal;
 
-   
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal , ICategoryDal categoryDal)
         {
             _productDal = productDal;
+            _categoryDal = categoryDal;
         }
-        //Note cross cutting concerns (01:44:12-12.Day)
-            //Log - Cache - Transcation - Authorization - Performance 
-        //business codes c= validation
 
+        */
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
+        {
+            _productDal = productDal;
+            _categoryService = categoryService;
+        }
+
+
+        //Note cross cutting concerns (01:44:12-12.Day)
+        //Log - Cache - Transcation - Authorization - Performance 
+        //business codes c= validation
+        /*
+         İş kurallarını aşağıdaki gibi yazarsan ilerde illaki spagetti ye döner ve bir daha lazım olunca dry prensibi kırmış oluruz.
+
+        //iş kuralı parçası
+         var result = _productDal.GetAll(p => p.CategoryId == product.CategoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError)
+            }
+
+            yukarıdaki kodu add blogunun içine yazmak yerine private method halinde bu classın içinde ayrı iş kural parçacığı şeklinde yazıp 
+            her yerde (sınıf içi) kullanabilirim.
+         */
+        /*
+             * if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
+            {
+                if (CheckIfProductNameExists(product.ProductName).Success)
+                {
+                    _productDal.Add(product);
+                    return new SuccessResult(Messages.ProductAdded);
+                }
+            }
+            */
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
+            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
+                CheckIfProductCountOfCategoryCorrect(product.CategoryId),CheckIfCategoryLimitExceded());
+            
+            //eğer mevcut category sayısı 15 i geçtiyse yeni ürün eklenemez
+            if (result != null)
+            {
+                return result;
+            }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
+            
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -70,5 +118,43 @@ namespace Business.Concrete
             }*/
             return new SuccessDataResult<List<ProductDetailDto>>( _productDal.GetProductDetail());
         }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            //Select Count(*) from products where categoryId=1
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count >15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+            return new SuccessResult();
+        }
+
+
     }
 }
--- BootcampFinalProject/Business/Constants/Messages.cs
+++ BootcampFinalProject/Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersizdir.";
         public static string MaintainceTime = "Sistem bakımdadır.";
         public static string ProductsListed = "Ürünler listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride 10'dan fazla ürn var. Ürün Eklenemedi.";
+        public static string ProductNameAlreadyExists = "Zaten bu isimde bir ürün bulunuyor.";
+        public static string CategoryLimitExceded = "Kategori sınırı aşıldı.";
     }
 }
