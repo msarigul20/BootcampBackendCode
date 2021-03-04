f509b6a8c6928c978797aa28bc43ce97451aaf50
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-05 01:03:50 +0300

    Added validator to add method for user.

--- ReCapProject/Business/Concrete/UserManager.cs
+++ ReCapProject/Business/Concrete/UserManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Entities.Concrete;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,7 +23,7 @@ namespace Business.Concrete
         {
             return _userDal.GetClaims(user);
         }
-
+        [ValidationAspect(typeof(UserValidator))]
         public void Add(User user)
         {
             _userDal.Add(user);
