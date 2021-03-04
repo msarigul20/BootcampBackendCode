403a28f53d0b01b7dd6623accf411181a0d1fd56
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-05 00:33:59 +0300

    Code clean up.

--- ReCapProject/Business/Concrete/CustomerManager.cs
+++ ReCapProject/Business/Concrete/CustomerManager.cs
@@ -12,24 +12,29 @@ namespace Business.Concrete
     public class CustomerManager: ICustomerService
     {
         ICustomerDal _customerDal;
+
         public CustomerManager(ICustomerDal customerDal)
         {
             _customerDal = customerDal;
         }
+
         public IResult Add(Customer customer)
         {
             _customerDal.Add(customer);
+
             return new SuccessResult(Messages.CustomerAdded);
         }
         public IResult Update(Customer customer)
         {
             _customerDal.Update(customer);
+
             return new SuccessResult(Messages.CustomerUpdated);
         }
 
         public IResult Delete(Customer customer)
         {
             _customerDal.Delete(customer);
+
             return new SuccessResult(Messages.CustomerDeleted);
         }
 
@@ -42,8 +47,5 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<Customer>(_customerDal.Get(c => c.Id == id));
         }
-
-       
-
     }
 }
