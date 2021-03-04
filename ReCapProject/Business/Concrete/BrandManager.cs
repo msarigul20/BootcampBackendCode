d1eb5159ee147fe327e26efcd09bd2c3c0ebcdc2
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-05 00:25:02 +0300

    Code clean up.

--- ReCapProject/Business/Concrete/BrandManager.cs
+++ ReCapProject/Business/Concrete/BrandManager.cs
@@ -22,38 +22,23 @@ namespace Business.Concrete
         [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
-            /*
-            if (brand.BrandName.Length < 2)
-            {
-                Console.WriteLine($"Brand did not add the database because the brand name must have 2 characters or more. " +
-                $"You entered :{brand.BrandName}.");
-                return new ErrorResult(Messages.BrandNameInvalid);
-            }
-            */
-            
             _brandDal.Add(brand);
             return new SuccessResult(Messages.BrandAdded);
-            //Console.WriteLine($"The brand that is id of {brand.BrandId} has been added succesfully.");
-
         }
+
         public IResult Update(Brand brand)
         {
             if (brand.BrandName.Length < 2)
             {
                 return new ErrorResult(Messages.BrandNameInvalid);
-               /* Console.WriteLine($"Brand did not uptade the database because the brand name must have 2 characters or more. " +
-                    $"You entered :{brand.BrandName}."); */
             }
             _brandDal.Update(brand);
             return new SuccessResult(Messages.BrandUpdated);
-            //Console.WriteLine($"The brand that is id of {brand.BrandId} has been updated succesfully.");
-
         }
 
         public IResult Delete(Brand brand)
         {
             _brandDal.Delete(brand);
-            //Console.WriteLine($"The brand that is id of {brand.BrandId} has been deleted succesfully.");
             return new SuccessResult(Messages.BrandDeleted);
         }
 
@@ -67,6 +52,5 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<Brand>(_brandDal.Get(b => b.BrandId == id));
         }
-        
     }
 }
