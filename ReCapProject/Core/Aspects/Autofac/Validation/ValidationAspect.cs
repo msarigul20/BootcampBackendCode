af8684a50245d389dfce6f9b39f6ba548b946ca0
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-05 01:12:36 +0300

    Code clean up.

--- ReCapProject/Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ ReCapProject/Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -12,6 +12,7 @@ namespace Core.Aspects.Autofac.Validation
     public class ValidationAspect:MethodInterception
     {
         private Type _validatorType;
+
         public ValidationAspect(Type validatorType)
         {
             if (!typeof(IValidator).IsAssignableFrom(validatorType))
@@ -21,11 +22,13 @@ namespace Core.Aspects.Autofac.Validation
 
             _validatorType = validatorType;
         }
+
         protected override void OnBefore(IInvocation invocation)
         {
             var validator = (IValidator)Activator.CreateInstance(_validatorType);
             var entityType = _validatorType.BaseType.GetGenericArguments()[0];
             var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
+
             foreach (var entity in entities)
             {
                 ValidationTool.Validate(validator,entity);
