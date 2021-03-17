078f3c26fdeac2eb764858a00ffe82bd386f7a37
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-17 21:14:26 +0300

    Created new controller

--- BootcampFinalProject/WebAPI/Controllers/CategoriesController.cs
+++ BootcampFinalProject/WebAPI/Controllers/CategoriesController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] 
     [ApiController]
     public class CategoriesController : ControllerBase
     {
