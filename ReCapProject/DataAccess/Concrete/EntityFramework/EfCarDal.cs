987c7050bcf1ffa5f897a73c4aec116d043216c1
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-19 14:12:15 +0300

    Updated detail method with expression function to filter data which is coming from database.

--- ReCapProject/DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -13,11 +13,11 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, CarDbContext>, ICarDal
     {
-        public List<CarDetailDto> GetCarDetails()
+        public List<CarDetailDto> GetCarDetails(Expression<Func<Car, bool>> filter = null)
         {
             using (CarDbContext context = new CarDbContext())
             {
-                var result = from car in context.Cars
+                var result = from car in filter == null ? context.Cars : context.Cars.Where(filter)
                              join color in context.Colors on car.ColorId equals color.ColorId
                              join brand in context.Brands on car.BrandId equals brand.BrandId
                              select new CarDetailDto()
