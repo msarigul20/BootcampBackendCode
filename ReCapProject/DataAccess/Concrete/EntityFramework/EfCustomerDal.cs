bf20d5841469ef3a01b159e345e24612c1015502
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-04-10 16:39:15 +0300

    Added customer detail method as dto

--- ReCapProject/DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
+++ ReCapProject/DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -1,11 +1,35 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCustomerDal : EfEntityRepositoryBase<Customer,CarDbContext>, ICustomerDal { }
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer, CarDbContext>, ICustomerDal
+    {
+        public List<CustomerDetailDto> GetCustomerDetails(Expression<Func<Customer, bool>> filter = null)
+        {
+            using (CarDbContext context = new CarDbContext())
+            {
+                var result = from customer in filter == null ? context.Customers : context.Customers.Where(filter)
+                             join user in context.Users on customer.UserId equals user.Id
+ 
+                             select new CustomerDetailDto()
+                             {
+                                 CustomerId = customer.Id,
+                                 CompanyName= customer.CompanyName,
+                                 FirstName = user.FirstName,
+                                 LastName = user.LastName,
+                                 Email = user.Email,
+                                 Password = user.PasswordHash                               
+                             };
+                return result.ToList();
+            }
+        }
+    }
 }
